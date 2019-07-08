b7bf7274c4d017c26eba8bf838d382e56eff4978
Ryan Kurte <ryankurte@gmail.com>
2019-07-08 22:12:09 +1200

    working on simple integration test

--- tests/integration.rs
+++ tests/integration.rs
@@ -3,20 +3,28 @@
 //! Copyright 2019 Ryan Kurte
 
 use std::collections::HashMap;
+use std::thread;
+use std::time::Duration;
 
 extern crate embedded_spi;
-use embedded_spi::wrapper::Wrapper;
 use embedded_spi::utils::{DeviceConfig, load_config};
 
 extern crate radio_sx127x;
-use radio_sx127x::{Sx127x, Settings};
+use radio_sx127x::{Sx127x, Settings, lora::Info, lora::LoRaConfig};
+
+extern crate radio;
+use radio::{Receive, Transmit};
 
 #[test]
 #[ignore]
 fn integration() {
     // Fetch configuration file name
-    let config_file = std::env::var("SX127x_TEST_CONFIG")
-        .expect("Integration test conifiguratin (SX127x_TEST_CONFIG) must be specified");
+    let config_file = match std::env::var("SX127x_TEST_CONFIG") {
+        Ok(v) => v,
+        Err(_e) => "./configs/pi-ci-sx127x.yml".to_owned(),
+    };
+
+    println!("Using configuration file: {}", config_file);
 
     // Load configurations from file
     let configs = load_config::<HashMap<String, DeviceConfig>>(&config_file);
@@ -27,11 +35,49 @@ fn integration() {
     let (w1, w2) = (config1.load(), config2.load());
 
     let settings1 = Settings::default();
-    let mut radio1 = Sx127x::new(w1, settings1).expect("error creating radio1");
+    let radio1 = Sx127x::new(w1, settings1).expect("error creating radio1");
+    let mut radio1 = radio1.lora(LoRaConfig::default()).unwrap();
 
     let settings2 = Settings::default();
-    let mut radio2 = Sx127x::new(w2, settings2).expect("error creating radio1");
+    let radio2 = Sx127x::new(w2, settings2).expect("error creating radio1");
+    let mut radio2 = radio2.lora(LoRaConfig::default()).unwrap();
 
+    println!("Testing send/receive");
 
+    let data = &[0xaa, 0xbb, 0xcc];
+
+    // Configure receive
+    radio1.start_receive().unwrap();
+
+    // Start transmit
+    radio2.start_transmit(data).unwrap();
+
+    // Poll on tx and rx complete
+    let mut sent = false;
+    let mut received = false;
+    let mut buff = [0u8; 1024];
+    let mut n = 0;
+    let mut info = Info::default();
+
+    for _i in 0..10 {
+        // Check TX state
+        if radio2.check_transmit().unwrap() {
+            println!("TX complete");
+            sent = true;
+        }
+
+        // Check RX state
+        if radio1.check_receive(false).unwrap() {
+            n = radio1.get_received(&mut info, &mut buff).unwrap();
+            received = true;
+            println!("RX complete ({:?} {:?})", info, &buff[..n]);
+        }
+
+        thread::sleep(Duration::from_millis(100));
+    }
+
+    assert!(sent, "Send not completed");
+    assert!(received, "Receive not completed");
+    assert_eq!(data, &buff[..n]);
 
 }
